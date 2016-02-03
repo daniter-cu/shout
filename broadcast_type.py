aa6d654664c6acde62939e01953b03890e306cbb
Kesler Tanner <keslert@gmail.com>
2016-02-03 14:23:49 -0800

    Add broadcast_type

---
+++ broadcast_type.py
@@ -0,0 +1,9 @@
+class BroadcastType(object):
+    message 	= "msg:"
+    ping        = "ping:"
+    renameChat  = "room:"
+    renameUser  = "name:"
+    help        = "help"
+
+    def values(self):
+        return {value for key, value in self.__dict__ if not key.startswith('__') and not callable(key)}
