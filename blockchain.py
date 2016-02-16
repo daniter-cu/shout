5912a79e28df2315ce4b14c215bb3e0dfa54333f
Dan Iter <daniter@stanford.edu>
2016-02-16 15:37:18 -0800

    Added timeout for proposal so users cant over write each other

--- blockchain.py
+++ blockchain.py
@@ -1,8 +1,19 @@
+from time import time
+
 class Blockchain:
     def __init__(self):
         self.items = []
         self.proposedBlock = None
+        self.timestamp = time()
 
+    def proposal_allowed(self):
+        if self.proposedBlock is None:
+            return True # no proposed block
+        if not self.is_empty() and self.proposedBlock.hash() == self.peek().hash():
+            return True # accepted block
+        if time() - self.timestamp > 3: # 3 second timeout
+            return True
+        return False
 
     def propose_block(self, block):
         self.proposedBlock = block
@@ -11,6 +22,7 @@ class Blockchain:
         # Note that you might be compeled to just use the self.proposedBlock
         # but htis is not always the correct block
         self.__push(block)
+
         #self.proposedBlock = None
 
     def is_next_block_proposed(self):
